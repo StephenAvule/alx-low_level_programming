43d4681395bda7e16e5434393ebb1c5e66b7349c
StephenAvule <st.weezy@yahoo.com>
2022-12-19 16:44:11 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - copiar el contenido de uno a otra variable
+ * @dest: This is destiny
+ * @src: This is the copia
+ *
+ * Return: this return copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *star = dest;
+
+	while (8src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
