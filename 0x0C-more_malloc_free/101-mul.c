0437e8eeba621e31818ea3e66ce4f601f5e83f9c
StephenAvule <st.weezy@yahoo.com>
2023-01-13 06:01:16 -0800

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
- * *main - multiplies two positive numbers
+*main - multiplies two positive numbers
 *@argc: n arguments
 *@argv: args
 *Return: int
 */
+
 int main(int argc, char *argv[])
 {
+
 unsigned long mul;
 int i, j;
 if (argc != 3)
